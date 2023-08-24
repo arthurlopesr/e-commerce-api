b1906bdb99f8685067dcc5a898dcef3ff86a8fcd
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-24 20:43:03 -0300

    feat: crate user Post

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,6 +5,9 @@ import { CreateUserDto } from './dto/createUser.dto';
 export class UserController {
   @Post()
   async createUser(@Body() createUser: CreateUserDto) {
-    return createUser;
+    return {
+      ...createUser,
+      password: undefined,
+    };
   }
 }
